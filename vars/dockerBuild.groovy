5447768a5bb050053e309c3ee424fc260f246a67
vikalsi <149544888+vikalsi@users.noreply.github.com>
2024-12-21 10:39:52 -0500

    Update dockerBuild.groovy

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -1,14 +1,20 @@
-def call(String project, String hubUser) {
-    sh "sudo DOCKER_BUILDKIT=1 docker build -t ${hubUser}/${project} ."
-    sh "sudo docker tag ${hubUser}/${project} ${hubUser}/${project}:${ImageTag}"
-    sh "sudo docker tag ${hubUser}/${project} ${hubUser}/${project}:latest"
-    withCredentials([usernamePassword(
-            credentialsId: "docker_cred",
-            usernameVariable: "USER",
-            passwordVariable: "PASS"
-    )]) {
-        sh "docker login -u '$USER' -p '$PASS'"
+def call(String project, String hubUser, String ImageTag) {
+    echo "Building image for ${hubUser}/${project}:${ImageTag}"
+    withEnv(["DOCKER_BUILDKIT=1"]) {
+        sh "docker build -t ${hubUser}/${project} ."
+        sh "docker tag ${hubUser}/${project} ${hubUser}/${project}:${ImageTag}"
+        sh "docker tag ${hubUser}/${project} ${hubUser}/${project}:latest"
+
+        withCredentials([usernamePassword(
+                credentialsId: "docker_cred",
+                usernameVariable: "USER",
+                passwordVariable: "PASS"
+        )]) {
+            echo "Logging into Docker Hub"
+            sh "docker login -u '$USER' -p '$PASS'"
+        }
+        echo "Pushing image ${hubUser}/${project}:${ImageTag}"
+        sh "docker image push ${hubUser}/${project}:${ImageTag}"
+        sh "docker image push ${hubUser}/${project}:latest"
     }
-    sh "sudo docker image push ${hubUser}/${project}:${ImageTag}"
-    sh "sudo docker image push ${hubUser}/${project}:latest"
 }
