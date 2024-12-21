dfb22fd22c256306a7c6aba2ee9f7c417eed906f
vikalsi <149544888+vikalsi@users.noreply.github.com>
2024-12-21 10:29:49 -0500

    Update dockerBuild.groovy

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -1,7 +1,7 @@
 def call(String project, String hubUser) {
     sh "sudo DOCKER_BUILDKIT=1 docker build -t ${hubUser}/${project} ."
-    sh "docker tag ${hubUser}/${project} ${hubUser}/${project}:${ImageTag}"
-    sh "docker tag ${hubUser}/${project} ${hubUser}/${project}:latest"
+    sh "sudo docker tag ${hubUser}/${project} ${hubUser}/${project}:${ImageTag}"
+    sh "sudo docker tag ${hubUser}/${project} ${hubUser}/${project}:latest"
     withCredentials([usernamePassword(
             credentialsId: "docker_cred",
             usernameVariable: "USER",
@@ -9,6 +9,6 @@ def call(String project, String hubUser) {
     )]) {
         sh "docker login -u '$USER' -p '$PASS'"
     }
-    sh "docker image push ${hubUser}/${project}:${ImageTag}"
-    sh "docker image push ${hubUser}/${project}:latest"
+    sh "sudo docker image push ${hubUser}/${project}:${ImageTag}"
+    sh "sudo docker image push ${hubUser}/${project}:latest"
 }
