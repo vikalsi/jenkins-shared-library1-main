f07ffbd792df8a0c46ecb9286393f871c8632f06
vikalsi <149544888+vikalsi@users.noreply.github.com>
2024-12-20 23:32:09 -0500

    update docker build cmd

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -1,5 +1,5 @@
 def call(String project, String hubUser) {
-    sh "docker image build -t ${hubUser}/${project} ."
+    sh "DOCKER_BUILDKIT=1 docker build -t ${hubUser}/${project} ."
     sh "docker tag ${hubUser}/${project} ${hubUser}/${project}:${ImageTag}"
     sh "docker tag ${hubUser}/${project} ${hubUser}/${project}:latest"
     withCredentials([usernamePassword(
